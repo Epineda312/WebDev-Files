bae394bb79f0622718e3754ec11a7684908932c2
Airic <epineda773@protonmail.com>
2019-11-25 23:59:40 -0600

    Update problemSolutions.js

--- JavaScriptSection/tweetCharacterCounter/problemSolutions.js
+++ JavaScriptSection/tweetCharacterCounter/problemSolutions.js
@@ -45,3 +45,4 @@ var dogAge = prompt("How old is your dog?");
 var humanAge = (dogAge - 2) * 4 + 21;
 alert("You dogAge converted to human years is " + humanAge);
 //------------------------------End Problem 4-------------------------------------//
+getmilk();
