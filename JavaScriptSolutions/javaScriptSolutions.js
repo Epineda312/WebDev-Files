6a5566fb91f122cf8d13153650c8fe9961aabf40
Airic <epineda773@protonmail.com>
2019-12-20 00:33:20 -0600

    Add files via upload
    Added Fibonacci problem as well as multiple solutions

--- JavaScriptSolutions/javaScriptSolutions.js
+++ JavaScriptSolutions/javaScriptSolutions.js
@@ -5,14 +5,12 @@ var tweetCount = tweet.length;
 alert("You have written " + tweetCount + " characters, you have " + (140 - tweetCount) + " characters remaining");
 //------------------------------End Problem 1-------------------------------------//
 
-
 //------------------------------Start Problem 2-------------------------------------//
 //Slices extra characters from tweet after 140 characters
 var tweet = prompt("Compose Your Tweet");
 var tweetUnder140 = tweet.slice(0,140);
 alert(tweetUnder140);
 //------------------------------End Problem 2-------------------------------------//
-
 
 //------------------------------Start Problem 3-------------------------------------//
 //1 Create a var that stores the name that user enters via prompt
@@ -85,21 +83,191 @@ if(loveScore > 70){
   alert("Your love score is "+ loveScore + "%" +
         " \nSheesh, You go together like oil and water");
 }
-
 //------------------------------End Problem 6-------------------------------------//
+
 //------------------------------Start Problem 7-------------------------------------//
+//Create a BMI calculator complete with if statements that generate different outputs
+//depending on the BMI
 function bmiCalculator (weight, height) {
     var bmi = weight/(height * height);
-    return Math.round(bmi)
+    var interpretation = "";
+
     if(bmi < 18.5){
-        alert("You are underweight");
+        interpretation = ("Your BMI is "+ bmi +", so you are underweight.");
     }
-     if(bmi >= 18.5 %% bmi <= 24.9){
-        alert("You are normal weight")
+     if(bmi >= 18.5 && bmi <= 24.9){
+        interpretation = ("Your BMI is "+ bmi +", so you have a normal weight.");
     }
      if(bmi > 24.9){
-        alert("You are overweight")
+        interpretation = ("Your BMI is "+ bmi +", so you are overweight.");
     }
     return interpretation;
 }
 //------------------------------End Problem 7-------------------------------------//
+
+//------------------------------Start Problem 8-------------------------------------//
+//Create an array of guest names then prompt the user and ask for their name
+//Check guest list to see if the users name is on the list
+var guestList = ["Angela", "Jack", "Pam", "James", "Lara", "Jason"];
+
+var guestName = prompt("What is your name?")
+guestList.includes(guestName);
+
+if(guestList.includes(guestName) == true){
+  alert("Your name is on the guest List");
+}else{
+  alert("Your name is not on the guest list");
+}
+//------------------------------End Problem 8-------------------------------------//
+
+//------------------------------Start Problem 9-------------------------------------//
+/*Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz"
+instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of
+both three and five print "FizzBuzz"*/
+var output = [];
+var count = 1;
+
+function fizzBuzz(){
+//write code here
+while(count <= 100){
+  if(count % 3 === 0 && count % 5 === 0 ) {
+    output.push("FizzBuzz");
+  }
+  else if(count % 3 === 0){
+    output.push("Fizz");
+  }
+  else if(count % 5 === 0){
+    output.push("Buzz");
+  }
+  else{
+    output.push(count);
+  }
+  count++;
+  }
+  console.log(output)
+}
+//------------------------------End Problem 9-------------------------------------//
+
+//------------------------------Start Problem 10-------------------------------------//
+//Use a while loop to print out the 99 bottles of beer on the wall song
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottles";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottle";
+    } else if(numberOfBottles === 0){
+      bottleWord = "bottles"
+    }
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+   numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
+//------------------------------End Problem 10-------------------------------------//
+
+//------------------------------Start Problem 10-------------------------------------//
+//FizzBuzz problem but as a For loop instead of a while loop
+
+//Increment for loop
+var output = [];
+var count = 1;
+
+function fizzBuzz(){
+//write code here
+for(count = 1; count < 101; count++;){
+  if(count % 3 === 0 && count % 5 === 0 ) {
+    output.push("FizzBuzz");
+  }
+  else if(count % 3 === 0){
+    output.push("Fizz");
+  }
+  else if(count % 5 === 0){
+    output.push("Buzz");
+  }
+  else{
+    output.push(count);
+  }
+  count++;
+  }
+  console.log(output)
+}
+
+//Decrement for loop
+var output = [];
+var count = 1;
+
+function fizzBuzz(){
+//write code here
+for(count = 100; count > 1; count--;) {
+  if(count % 3 === 0 && count % 5 === 0 ) {
+    output.push("FizzBuzz");
+  }
+  else if(count % 3 === 0){
+    output.push("Fizz");
+  }
+  else if(count % 5 === 0){
+    output.push("Buzz");
+  }
+  else{
+    output.push(count);
+  }
+  count++;
+  }
+  console.log(output)
+}
+//------------------------------End Problem 11-------------------------------------//
+
+//------------------------------Start Problem 12-------------------------------------//
+//fibonacci Challenge
+//Expected solution
+function fibonacciGenerator(n){
+   var a=[];
+
+   if(n===1){
+        a.push(0);
+    }
+
+   else if(n>=2){
+        a.push(0);
+        a.push(1);
+    }
+
+    for(var i=2;i<n;i++){
+        a[i]=a[i-1]+a[i-2];
+    }
+
+  return a;
+}
+
+//Different solution
+function fibonacci(num){
+  var a = 1, b = 0, temp;
+
+  while (num >= 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
+
+  return b;
+}
+
+//Different solution
+function fibonacciGenerator (n) {
+    //TODO
+    array =[];
+    for (i=1;i<=n;i++) {
+        if ( array.length <= 1 ) {
+            array.push(i-1);
+        } else {
+            array.push((array[i-3]+array[i-2]));
+        }
+        if ( i === n) {
+            return array;
+        }
+    }
+}
+fibonacciGenerator(1)
+//------------------------------End Problem 12-------------------------------------//
