9e1e883f352b349b3013a15693e549fee1f8c9d4
Airic <epineda773@protonmail.com>
2019-12-22 01:39:09 -0600

    Add files via upload
    Shortened the code

--- JavaScriptSolutions/Dice Game/index.js
+++ JavaScriptSolutions/Dice Game/index.js
@@ -4,7 +4,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomImageSource = "images/dice" + randomNumber1 + ".png"; // images/dice1.png - images/dice6.png
 
-image1.document.querySelectorAll("img")[0].setAttribute("src", randomImageSource); //
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource); //
 
 
 //------------------------Second Image-----------------------//
