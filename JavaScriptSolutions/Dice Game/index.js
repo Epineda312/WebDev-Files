ac5d4cabe95885784750b3e25b09e484504aebd6
Airic <epineda773@protonmail.com>
2019-12-22 01:31:07 -0600

    Add files via upload
    Working Dice Game

--- JavaScriptSolutions/Dice Game/index.js
+++ JavaScriptSolutions/Dice Game/index.js
@@ -13,10 +13,20 @@ image1.setAttribute("src", randomImageSource); //
 
 //------------------------Second Image-----------------------//
 
-var randomNumber2 = Math.floor(Math.random) * 6) + 1 //1-6
+var randomNumber2 = Math.floor(Math.random() * 6) + 1 //1-6
 
-var randomImageSource2 = "images/dice" + randomNumber2; //Scrolls through 6 dice images
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //Scrolls through 6 dice images
 
-var image2 = document.querySelectorAll("img")[1]; //
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-image2.setAttribute("src", randomImageSource); //
+
+//------------------------Logic-----------------------//
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "It's a draw!";
+}
