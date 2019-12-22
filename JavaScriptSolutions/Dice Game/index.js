95baec789a4d87f184ea1056cb194637f6348b23
Airic <epineda773@protonmail.com>
2019-12-22 01:34:08 -0600

    Add files via upload

--- JavaScriptSolutions/Dice Game/index.js
+++ JavaScriptSolutions/Dice Game/index.js
@@ -2,13 +2,9 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomImageSource = "images/dice" + randomNumber1 + ".png"; // images/dice1.png - images/dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; // images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0]; //
-
-image1.setAttribute("src", randomImageSource); //
+image1.document.querySelectorAll("img")[0].setAttribute("src", randomImageSource); //
 
 
 //------------------------Second Image-----------------------//
@@ -28,5 +24,5 @@ else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
 }
 else {
-  document.querySelector("h1").innerHTML = "It's a draw!";
+  document.querySelector("h1").innerHTML = "It's a Draw!";
 }
