ca0bdd25045f3da1c28e6d31c35be8d289eae442
Airic <epineda773@protonmail.com>
2019-12-24 04:44:03 -0600

    Add files via upload

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,3 +1,21 @@
-function handleClick(){
-  alert("I got clicked!");
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+
+for(var i = 0; i < numberOfDrumButtons; i++)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click", function()
+  {
+    this.style.color = "white";
+
+
+    // var audio = new Audio('sounds/tom-1.mp3');
+    // audio.play();
+  });
 }
+
+// for(var i = 0; i < document.querySelectorAll(".drum").length){
+// document.querySelectorAll("button")[].addEventListener("click", function() {
+//     alert("I Got Clicked!");
+//   }
+// }
