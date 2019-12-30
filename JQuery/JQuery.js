a9a1781fca88ad892ce6ba8536998c619f709ddc
Airic <epineda773@protonmail.com>
2019-12-29 21:10:49 -0600

    Add files via upload
    Manipulating HTML elements using JQuery practice.

--- JQuery/JQuery.js
+++ JQuery/JQuery.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-  $("h1").css("color", "red");
-});
+// $(document).ready(function() {
+//   $("h1").css("color", "red");
+// }); // changes h1 to red even if JQuery is declared in the head of the document
+
+// $("h1").css("font-size"); //Gets the value
+// $("h1").css("font-size", "5rem"); //Sets the value
+
+// $("h1").addClass("big-title"); //adds class made in styles.css
+// $("h1").addClass("big-title margin-50"); //adds two classes
