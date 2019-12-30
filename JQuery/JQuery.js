af83f09cf1f565827d470f4d5cc847d645b00168
Airic <epineda773@protonmail.com>
2019-12-29 21:31:18 -0600

    Add files via upload
    More practice manipulating HTML using JQuery

--- JQuery/JQuery.js
+++ JQuery/JQuery.js
@@ -7,3 +7,8 @@
 
 // $("h1").addClass("big-title"); //adds class made in styles.css
 // $("h1").addClass("big-title margin-50"); //adds two classes
+
+// $("h1").text("Bye"); //Changes h1 text to "Bye"
+// $("Button").text("Don't Click Me"); //Changes button text to "Don't click me"
+$("Button").html("<em>Hey</em>"); // Changes button to emphasized Hey
+$("Button").text("<em>Hey</em>") // changes button text to <em>Hey</em>
