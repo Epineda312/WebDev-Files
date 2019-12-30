eb0ab23d818dbe1e451d2bf5767dc687083e0e10
Airic <epineda773@protonmail.com>
2019-12-29 21:42:26 -0600

    Add files via upload

--- JQuery/JQuery.js
+++ JQuery/JQuery.js
@@ -10,5 +10,18 @@
 
 // $("h1").text("Bye"); //Changes h1 text to "Bye"
 // $("Button").text("Don't Click Me"); //Changes button text to "Don't click me"
-$("Button").html("<em>Hey</em>"); // Changes button to emphasized Hey
-$("Button").text("<em>Hey</em>") // changes button text to <em>Hey</em>
+// $("Button").html("<em>Hey</em>"); // Changes button to emphasized Hey
+// $("Button").text("<em>Hey</em>") // changes button text to <em>Hey</em>
+
+// console.log($("img").attr("src")); //diplays src of image in console
+// $("a").attr("href", "https://www.yahoo.com"); //changes google link to yahoo
+
+$("h1").click(function(){
+  $("h1").css("color", "purple");
+}); //Changes h1 color to purple on click
+
+for(var i = 0; i<5; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function(){
+    document.querySelector("h1").style.color = "purple";
+  });
+} //changes h1 color to purple when any buttons are clicked
