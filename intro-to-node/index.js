309af805c9d36b16cc57fe99d04ec85f5cd82fc5
Airic <epineda773@protonmail.com>
2020-01-02 23:08:01 -0600

    Add files via upload
    Implemented supervillains npm. When code is ran a super hero name as well as a super villain name is generated. Code to display all potential super hero names as well as all super villain names is implemented but commented out.

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
 
+//--------------------------------Super-Heroes-----------------------//
 const superheroes = require('superheroes');
-
  /*
 superheroes.all;
 //=> ['3-d Man', 'A-Bomb', ...]
@@ -14,6 +14,26 @@ superheroes.random();
 var mySuperHeroName = superheroes.random();
 console.log(mySuperHeroName);
 
+
 //Display all potential random super hero names
 var allSuperHeroNames = superheroes.all;
-//console.log(allSuperHeroNames);
+// console.log(allSuperHeroNames);
+
+
+//--------------------------------Super-Villains-----------------------//
+const supervillains = require('supervillains');
+/*
+supervillains.all;
+//=> ['Abattoir', 'Able Crown', …]
+
+supervillains.random();
+//=> 'Mud Pack'
+*/
+
+//Displays a random super villain name each time code is ran
+var superVillainName = supervillains.random();
+console.log(superVillainName);
+
+//Display all potential super villain names
+var allSuperVillainNames = supervillains.all;
+// console.log(allSuperVillainNames);
