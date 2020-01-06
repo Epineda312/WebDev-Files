e24e303f4a491d7b0db5a020ba807e9dcf054cf3
Airic <epineda773@protonmail.com>
2020-01-05 23:49:40 -0600

    Add files via upload

--- expressServer/server.js
+++ expressServer/server.js
@@ -8,6 +8,10 @@ app.get("/", function(request, response/*req, res*/){
   response.send("<h1>Hello World!</h1>");
 });
 
+app.get("/contact", function(req, red){
+    res.send("Contact me at: Eric@gmail.com");
+});
+
 app.listen(3000, function(){
   console.log("server started on port 3000");
 });
