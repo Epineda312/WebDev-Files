e8c702c3b5e66066967db90c21faa65e036475f2
Airic <epineda773@protonmail.com>
2020-01-08 23:47:59 -0600

    Add files via upload
    BMI calculator so far

---
+++ calc/calc.js
@@ -0,0 +1,51 @@
+//jshint esversion:6
+
+//require express
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//setup express
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Home Route
+app.get("/", function(req, res /*request, response*/) {
+  //res.send("Hello User");
+  res.sendFile(__dirname + "/index.html");
+  // console.log(__dirname);
+});
+
+app.post("/", function(req, res){
+
+var num1 = Number(req.body.n1);
+var num2 = Number(req.body.n2);
+
+var result = num1 + num2;
+
+  res.send("The result of the calculation is " + result);
+});
+
+app.listen(3000, function(){
+  console.log("Server now listening on port 3000");
+});
+//------------------------BMI CALCULATOR--------------------------
+//Home Route
+app.get("/bmiCalculator", function(req, res /*request, response*/) {
+  //res.send("Hello User");
+  res.sendFile(__dirname + "/bmiCalculator.html");
+  // console.log(__dirname);
+});
+
+app.post("/bmiCalculator", function(req, res){
+
+var weight = Number(req.body.n1);
+var height = Number(req.body.n2);
+
+var result = num1 + num2;
+
+function bmiCalculator(weight, height){
+  var bmi = weight / (height * height);
+  return Math.round(bmi);
+}
+  res.send("Your BMI is " + result);
+});
