cd9d4cb9caf83cdd361c7e2273f15b740b7aed3b
Airic <epineda773@protonmail.com>
2020-01-31 23:54:08 -0600

    Add files via upload

--- Bitcoin-Ticker/index.js
+++ Bitcoin-Ticker/index.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
@@ -10,6 +11,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req,res){
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  // console.log(req.body.crypto);
+
+  request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
+    console.log(body);
+  });
+
 });
 
 app.listen(3000, function(){
