09d3a68d75bff09196fd823e1294244a92b6abd3
Airic <epineda773@protonmail.com>
2020-02-06 16:00:58 -0600

    Add files via upload
    current version

--- Bitcoin-Ticker/index.js
+++ Bitcoin-Ticker/index.js
@@ -17,11 +17,11 @@ app.post("/", function(req, res){
   // console.log(req.body.crypto);
 
   request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
-    console.log(body);
+    console.log(response.statusCode);
   });
 
 });
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.")
-})
+});
