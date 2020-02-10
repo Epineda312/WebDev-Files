36827a99aa210333a1e145eb04776b1c911ff73c
Airic <epineda773@protonmail.com>
2020-02-09 23:00:12 -0600

    Add files via upload

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -6,9 +6,17 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function(req, res){
-  res.send("Hello");
+
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  if(currentDay === 6 || currentDay === 0){
+    res.send("Yay it's the weekend!");
+  } else{
+    res.send("Boo! I have to work!");
+  }
 });
 
-app.listen(300, function(){
+app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
