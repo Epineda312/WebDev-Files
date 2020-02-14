f69298abe53c35b4b79c196472695b341f697dba
Airic <epineda773@protonmail.com>
2020-02-14 02:35:00 -0600

    Add files via upload
    Workaround for bitcoin average. Now working for all 3 cryptos (BTC,ETH,LTC) and all 3 fiat currencys (USD,GBP, EUR)

--- Bitcoin-Ticker/index.js
+++ Bitcoin-Ticker/index.js
@@ -3,38 +3,54 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-
+const options = {
+  url: "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD",
+  headers: {
+    "X-testing": "testing"
+  }
+};
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
-
-  // console.log(req.body.crypto);
+app.post("/", function(req, res) {
 
   var crypto = req.body.crypto;
   var fiat = req.body.fiat;
+  console.log(crypto);
+  console.log(fiat);
 
-  var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
-  let finalURL = baseURL + crypto + fiat;
-
-
-  request(finalURL, function(error, response, body){
-
+request({
+  url: "https://apiv2.bitcoinaverage.com/indices/global/ticker/" + crypto + fiat,
+  headers: {
+    'x-ba-key': 'Y2NiZWNiYWE3OTY3NDMyNzk2ODY5YzVmZDczNWY1NGU'
+  }
+},
+function(error, response, body) {
+  if (!error && response.statusCode == 200) {
     var data = JSON.parse(body);
-    var price = data.averages.week;
+    var price = data.last;
+    console.log(data.last);
 
-    res.send("<h1>The price of "+ crypto + " is "+ price + fiat +"</h1>");
+    var currentDate = data.display_timestamp;
 
+    res.write("<p>The current date is " + currentDate + "</p>");
+    res.write("<h1>The current price of " + crypto + " is " + price + fiat + "</h1>");
+    res.send();
+  } else {
+    console.log(error);
+    res.send(error);
+  }
   });
-
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server is running on port 3000.")
 });
