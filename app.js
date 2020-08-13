43f88796a7e2919a248909771f9852a1a5ef9d88
Airic <epineda773@protonmail.com>
2020-08-12 21:13:24 -0500

    Add files via upload
    mongoDB driver practice

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-onst MongoClient = require('mongodb').MongoClient;
+//jshint esversion:6
+
+const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
 // Database Name
-const dbName = 'myproject';
+const dbName = 'fruitsDB';
 
 // Create a new MongoClient
 const client = new MongoClient(url);
@@ -17,5 +19,48 @@ client.connect(function(err) {
 
   const db = client.db(dbName);
 
-  client.close();
+  insertDocuments(db, function() {
+    client.close();
+  });
 });
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Insert some documents
+  collection.insertMany([
+    {
+      name : "Apple",
+      score: 8,
+      review: "Great fruit"
+    },
+    {
+      name : "Orange",
+      score: 6,
+      review: "Kinda sour"
+    },
+    {
+      name: "Banana",
+      score: 9,
+      review: "Great stuff!"
+    }
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+const findDocuments = function(db, callback){
+  //Get the documents collection
+  const colelction = db.collection = db.collection('fruits');
+  //find some documents
+  collection.find({}).toArray(function(err, docs){
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
