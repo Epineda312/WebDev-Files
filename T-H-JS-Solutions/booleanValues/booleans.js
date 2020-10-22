89a1eb34be035b583240d07f235b792a54956063
Airic <epineda773@protonmail.com>
2020-10-22 00:02:34 -0500

    Update booleans.js
    add comments

--- T-H-JS-Solutions/booleanValues/booleans.js
+++ T-H-JS-Solutions/booleanValues/booleans.js
@@ -1,13 +1,26 @@
+/*
+The number guessing game 
+stores a number between 1 and 10 
+and gives the player one attempt to 
+guess the number.
+*/
+
+// When the game begins, the guess is false.
 let correctGuess = false;
-const number =  5;
+const number =  5; //number to guess
 const guess = prompt("Guess a number between 1 and 10.");
 
+/*
+  1. Test if a player's guess amtches the number.
+  2. Change the value of correctGuess to true if they match
+*/
  if( +guess === number ){
     correctGuess = true;
 } else {
     correctGuess = false;
 }
-                     
+
+// Test if guess is correct and output response
 if (correctGuess === true ) {
   console.log('You guessed the number!');
 } else {
