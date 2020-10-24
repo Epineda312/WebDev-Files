02da4392b0b0591896c5d150df71aa1e95822935
Airic <epineda773@protonmail.com>
2020-10-24 01:10:02 -0500

    Add files via upload
    add solution to second part of random number challenge

--- T-H-JS-Solutions/randomNumberChallengeSolution/random.js
+++ T-H-JS-Solutions/randomNumberChallengeSolution/random.js
@@ -1,4 +1,6 @@
- // Collect input from a user
+/*
+My Solution
+// Collect input from a user
 let inputHigh = prompt("Give me a number to work with");
 
 // Convert the input to a number
@@ -9,3 +11,27 @@ let ranNum = Math.floor(Math.random() * highNumber) + 1;
 
 // Create a message displaying the random number
 console.log(`your random number is ${ranNum}. Picked between 1 and ${highNumber}`);
+*/
+
+/*
+My Solution for second part of problem. Take two inputs, highest and lowest number, and generate a random number between the two values provided.
+*/
+
+ // Collect input from a user
+let inputHigh = prompt("Give me the highest number you want to work with");
+let inputLow = prompt("Give me the lowest number you want to work with");
+
+
+// Convert the input to a number
+let highNumber = parseInt(inputHigh);
+let lowNumber = parseInt(inputLow);
+
+if(highNumber && lowNumber){
+// Use Math.random() and the user's number to generate a random number
+let ranNum = Math.floor(Math.random() * (highNumber - lowNumber + 1) + lowNumber);
+
+// Create a message displaying the random number
+console.log(`your random number is ${ranNum}. Picked between ${lowNumber} and ${highNumber}`);  
+} else {
+  console.log('You need to provide two numbers. Try again.');
+}
