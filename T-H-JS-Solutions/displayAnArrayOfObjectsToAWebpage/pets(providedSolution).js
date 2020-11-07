53e48bc638bb37c4524a77869de4df382dc24b8d
Airic <epineda773@protonmail.com>
2020-11-07 01:52:04 -0600

    Add files via upload
    minor changes to initial solution. This is more concise yet yields the same results. Addition of directory js file makes pets.js only hold the pets array. Overall it is easier to read and a better way to work with objects in an array.

---
+++ T-H-JS-Solutions/displayAnArrayOfObjectsToAWebpage/pets(providedSolution).js
@@ -0,0 +1,51 @@
+/*
+  Create an array of 'pet' objects.
+  Each object should have the following properties: 
+  name, type, breed, age, and photo
+*/
+
+const pets = [
+  {
+    name:'Cindy',
+    type:'Dog',
+    breed:'Aussie',
+    age:'3',
+    photo:'img/aussie.jpg',
+  },
+   {
+    name:'Kyle',
+    type:'Dog',
+    breed:'Dachshund',
+    age:'2',
+    photo:'img/dachshund.jpg',
+  },
+   {
+    name:'Conway',
+    type:'Dog',
+    breed:'Golden',
+    age:'4',
+    photo:'img/golden.jpg',
+  },
+   {
+    name:'Benny',
+    type:'Cat',
+    breed:'Persian',
+    age:'4',
+    photo:'img/persian.jpg',
+  },
+   {
+    name:'Nas',
+    type:'Dog',
+    breed:'Pug',
+    age:'4',
+    photo:'img/pug.jpg',
+  },
+   {
+    name:'Jay',
+    type:'Cat',
+    breed:'Tabby',
+    age:'3',
+    photo:'img/tabby.jpg',
+  },
+];
+  
