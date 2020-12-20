1019234a9c6e2c8cb50b309601ff4e43e936196e
Airic <epineda773@protonmail.com>
2020-12-19 20:44:55 -0600

    Add files via upload
    On top of the employee status widget, add a widget that tells the user which meeting rooms are currently full or empty. Parse the rooms.json file to find this information out and display it accordingly.

--- T-H-JS-Solutions/ajaxParsingJSON/widget.js
+++ T-H-JS-Solutions/ajaxParsingJSON/widget.js
@@ -1,13 +1,12 @@
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-  if(xhr.readyState === 4){
+xhr.onreadystatechange = function () {
+  if(xhr.readyState === 4 && xhr.status === 200) {
     var employees = JSON.parse(xhr.responseText);
-    console.log(employees);
     var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i<employees.length; i += 1){
-      if(employees[i].inoffice === true) {
+    for (var i=0; i<employees.length; i += 1) {
+      if (employees[i].inoffice === true) {
         statusHTML += '<li class="in">';
-      } else{
+      } else {
         statusHTML += '<li class="out">';
       }
       statusHTML += employees[i].name;
@@ -17,6 +16,26 @@ xhr.onreadystatechange = function(){
     document.getElementById('employeeList').innerHTML = statusHTML;
   }
 };
-
-xhr.open('GET', 'data/employees.json');
+xhr.open('GET', '../data/employees.json');
 xhr.send();
+
+var roomRequest = new XMLHttpRequest();
+roomRequest.onreadystatechange = function () {
+  if(roomRequest.readyState === 4 && roomRequest.status === 200) {
+    var rooms = JSON.parse(roomRequest.responseText);
+    var statusHTML2 = '<ul class="bulleted">';
+    for (var i=0; i<rooms.length; i += 1) {
+      if (rooms[i].available === true) {
+        statusHTML2 += '<li class="in">';
+      } else {
+        statusHTML2 += '<li class="out">';
+      }
+      statusHTML2 += rooms[i].room;
+      statusHTML2 += '</li>';
+    }
+    statusHTML2 += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML2;
+  }
+};
+roomRequest.open('GET', '../data/rooms.json');
+roomRequest.send();
