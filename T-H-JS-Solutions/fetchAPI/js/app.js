3e04a17831d83616f2011b02b253d3c9268a1e67
Airic <epineda773@protonmail.com>
2021-01-02 05:12:10 -0600

    Add files via upload
    The default HTTP method for making fetch requests is GET, which retrieves data from a server. You can make requests other than GET (like POST, DELETE and PUT) by passing additional information to fetch().

--- T-H-JS-Solutions/fetchAPI/js/app.js
+++ T-H-JS-Solutions/fetchAPI/js/app.js
@@ -5,20 +5,45 @@ const form = document.querySelector('form');
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-fetch('https://dog.ceo/api/breeds/list')
-    .then(response => response.json())
-    .then(data => generateOptions(data.message))
+function fetchData(url){
+    return fetch(url)
+        .then(checkStatus)
+        .then(res => res.json())
+        .catch(error => console.log('Looks like there was a problem', error))
     
+}
 
+Promise.all([
+    fetchData('https://dog.ceo/api/breeds/list'),
+    fetchData('https://dog.ceo/api/breeds/image/random')
+])
+.then(data => {
+    const breedList = data[0].message;
+    const randomImage = data[1].message;
+    
+    generateOptions(breedList);
+    generateImage(randomImage);
+})
 
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => generateImage(data.message))
+//Kept for reference, this code has been replaced by Promise.all
+//fetchData('https://dog.ceo/api/breeds/list')
+//    .then(data => generateOptions(data.message))
+//   
+//fetchData('https://dog.ceo/api/breeds/image/random')
+//    .then(data => generateImage(data.message))
 
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+function checkStatus(response){
+    if(response.ok) {
+        return Promise.resolve(response);
+       } else{
+        return Promise.reject(new Error(response.statusText));
+       }
+}
+
 function generateOptions(data){
     const options = data.map(item => `
         <option value='${item}'> ${item} </option>
@@ -35,13 +60,47 @@ function generateImage(data){
     card.innerHTML = html;
 }
 
+function fetchBreedImage(){
+    const breed = select.value;
+    const img = card.querySelector('img');
+    const p = card.querySelector('p');
+    
+    fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(data => {
+        img.src = data.message;
+        img.alt = breed;
+        p.textContent = `Click to view more ${breed}s`;
+    })
+}
+
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
-
+select.addEventListener('change', fetchBreedImage);
+card.addEventListener('click', fetchBreedImage);
+form.addEventListener('submit', postData);
 
 
 // ------------------------------------------
 //  POST DATA
 // ------------------------------------------
 
+function postData(e){
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const comment = document.getElementById('comment').value;
+    
+    const config = {
+    method:'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name, comment })
+}
+    
+    
+fetch('https://jsonplaceholder.typicode.com/comments', config) 
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(data => console.log(data)) 
+}
