51d948574f269c52cf318d894c3574ab5c32818e
Airic <epineda773@protonmail.com>
2021-01-11 02:55:26 -0600

    Add files via upload
    add createSpaces() and createPlayers() methods.

---
+++ T-H-JS-Solutions/fourInARowGameFiles/fourInARowGameFiles/js/Game.js
@@ -0,0 +1,19 @@
+class Game{
+    constructor(){
+        this.board = new Board;
+        this.players = this.createPlayers();
+        this.ready = false;
+    }
+    
+    /** 
+ * Creates two player objects
+ * @return  {Array}    An array of two Player objects.
+ */
+    createPlayers(){
+            const players = [new Player('Player1', 1, '#e15258', true),
+                             new Player('Player2', 2, 'e#59a13')];
+       
+        return players;
+    }
+}
+
