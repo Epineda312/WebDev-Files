4570869a492918ea54a825534d9577f7d481512e
Airic <epineda773@protonmail.com>
2021-01-21 23:25:06 -0600

    Add files via upload

---
+++ UD-JS/Fundamentals2/script.js
@@ -0,0 +1,50 @@
+/*
+//let js = 'amazing';
+//if (js === 'amazing') alert('JavaScript is FUN!');
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Eric");
+console.log(28);
+
+let firstName = "Eric";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+let person = "Airic";
+let PI = 3.1415;
+
+let myFirstJob = "programmer";
+let myCurrentJob = "teacher";
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Eric');
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+const job 
