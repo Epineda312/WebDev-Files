4e86b93b575532a013f87532a15d37b5b6a00b2d
Airic <epineda773@protonmail.com>
2021-09-02 22:29:00 -0500

    Add files via upload
    javascript functions notes

---
+++ WDCRe/Functions/app.js
@@ -0,0 +1,55 @@
+// JavaScript Functions
+
+// function helloWorld() {
+//     console.log('Hello world!');
+// }
+// helloWorld();
+
+// function sing() {
+//     console.log('LA LA LA');
+// }
+// sing();
+
+// function greet(person) {
+//     console.log(`person is: ${person}`)
+//     console.log(`Hi ${person}!`);
+// }
+
+// greet('Stee');
+
+// function rant(message) {
+//     console.log(message.toUpperCase())
+//     console.log(message.toUpperCase())
+//     console.log(message.toUpperCase())
+// }
+
+// rant('I hate beets');
+
+// function greet(firstName, lastName) {
+//     console.log(`Hey there ${firstName} ${lastName[0]}`);
+// }
+
+// greet('Stee', 'Pena');
+
+// function repeat(str, number) {
+//     let result = ' ';
+//     for (let i = 0; i < number; i++) {
+//         // console.log(`BINK: ${str}`)
+//         result += str;
+//     }
+//     console.log(result);
+// }
+
+// repeat('$', 5);
+
+// define isSnakeEyes below:
+// function isSnakeEyes(num1, num2) {
+//     if (num1 === 1 && num2 === 1) {
+//         console.log(`${num1} and ${num2} is snake eyes!`);
+//     } else {
+//         console.log(`${num1} and ${num2} is not snake eyes!`);
+//     }
+// }
+
+// isSnakeEyes(1, 1);
+// isSnakeEyes(1, 2);
