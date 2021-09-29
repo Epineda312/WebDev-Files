3b6e63cfda6511122befadb811d0351bf3cc2a36
Airic <epineda773@protonmail.com>
2021-09-28 21:29:26 -0500

    Add files via upload
    form events example. Make a ul list of tweets that get appended when submitted

---
+++ WDCRe/DOM-Events/FormEvents/app.js
@@ -0,0 +1,32 @@
+const tweetForm = document.querySelector('#tweetForm');
+const tweetsContainer = document.querySelector('#tweets');
+
+tweetForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    // const usernameInput = document.querySelectorAll('input')[0];
+    // const tweetInput = document.querySelectorAll('input')[1];
+
+    // const username = tweetForm.elements.username.value;
+    // const tweet = tweetForm.elements.tweet.value;
+
+    const usernameInput = tweetForm.elements.username;
+    const tweetInput = tweetForm.elements.tweet;
+
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+
+    addTweet(usernameInput.value, tweetInput.value);
+    usernameInput.value = ' ';
+    tweetInput.value = ' ';
+});
+
+const addTweet = (username, tweet) => {
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+
+    bTag.append(username);
+    newTweet.append(bTag);
+    newTweet.append(`-${tweet}`);
+    console.log(newTweet);
+    tweetsContainer.append(newTweet);
+}
