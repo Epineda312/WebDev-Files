f525fb1f4321845c5663115055d733fee5701239
Airic <epineda773@protonmail.com>
2021-10-02 15:47:35 -0500

    Add files via upload
    Refactored the Score Keeper code. Commented out replaced code for reference.

--- WDCRe/ScoreKeeper/app.js
+++ WDCRe/ScoreKeeper/app.js
@@ -1,10 +1,10 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
+// const p1Button = document.querySelector('#p1Button');
+// const p2Button = document.querySelector('#p2Button');
 const resetButton = document.querySelector('#reset');
 const winningScoreSelect = document.querySelector('#playTo');
 
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
+// const p1Display = document.querySelector('#p1Display');
+// const p2Display = document.querySelector('#p2Display');
 const wScore = document.querySelector('#playTo')
 
 let p1Score = 0;
@@ -12,32 +12,39 @@ let p2Score = 0;
 let winningScore = parseInt(wScore.value);
 let isGameover = false;
 
-p1Button.addEventListener('click', function () {
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+
+function updateScores(player, opponent) {
     if (!isGameover) {
-        p1Score++;
-        if (p1Score === winningScore) {
+        player.score += 1;
+        if (player.score === winningScore) {
             isGameover = true;
-            p1Display.classList.add('has-text-success');
-            p2Display.classList.add('has-text-danger');
-            p1Button.disabled = true;
-            p2Button.disabled = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.disabled = true;
+            opponent.disabled = true;
         }
-        p1Display.textContent = p1Score;
+        player.display.textContent = player.score;
     }
+}
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2);
 })
 
-p2Button.addEventListener('click', function () {
-    if (!isGameover) {
-        p2Score++;
-        if (p2Score === winningScore) {
-            isGameover = true;
-            p2Display.classList.add('has-text-success');
-            p1Display.classList.add('has-text-danger');
-            p1Button.disabled = true;
-            p2Button.disabled = true;
-        }
-        p2Display.textContent = p2Score;
-    }
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1);
 })
 
 winningScoreSelect.addEventListener('change', function () {
@@ -49,12 +56,20 @@ resetButton.addEventListener('click', reset)
 
 function reset() {
     isGameover = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-    p1Display.classList.remove('has-text-success', 'has-text-danger');
-    p2Display.classList.remove('has-text-success', 'has-text-danger');
-    p1Button.disabled = false;
-    p2Button.disabled = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
+
+    // p1.score = 0;
+    // p2.score = 0;
+    // p1.display.textContent = 0;
+    // p2.display.textContent = 0;
+    // p1.display.classList.remove('has-text-success', 'has-text-danger');
+    // p2.display.classList.remove('has-text-success', 'has-text-danger');
+    // p1.button.disabled = false;
+    // p2.button.disabled = false;
 }
+
